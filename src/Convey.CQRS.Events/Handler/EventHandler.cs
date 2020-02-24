0f73e29d078633dad7cca0acfba74ea8b302d7b4
fahri.goktuna <fahri.goktuna@turkcell.com.tr>
2020-02-24 12:50:36 +0300

    EventDispatcher DispatchAsync added

---
+++ src/Convey.CQRS.Events/Handler/EventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convey.CQRS.Events.Handler
+{
+    public abstract class EventHandler
+    {
+        public abstract Task Handle(IEvent @vent);
+    }
+
+    public class EventHandler<T> : EventHandler
+        where T : class, IEvent
+    {
+        private readonly IEventHandler<T> _handler;
+
+        public EventHandler(IEventHandler<T> handler)
+        {
+            _handler = handler;
+        }
+
+        public override async Task Handle(IEvent domainEvent)
+        {
+            await _handler.HandleAsync((T)domainEvent);
+        }
+    }
+}
